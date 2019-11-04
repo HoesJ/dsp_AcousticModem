f63ce4178ee9a52e8de3448c1f4c1cb29b8c88b6
Jochem Hoes <r0666420@pc-klas1-13.esat.kuleuven.be>
2019-11-04 10:00:38 +0100

    On-Off Bit loading

--- OFDM/main.m
+++ OFDM/main.m
@@ -1,4 +1,4 @@
-% Exercise session 4: DMT-OFDM transmission scheme
+%% Simple transmssion
 
 % Convert BMP image to bitstream
 [bitStream, imageData, colorMap, imageSize, bitsPerPixel] = imagetobitstream('image.bmp');
@@ -48,7 +48,43 @@ end
 figure
 plot(res)
 
-%% Adaptive bit loading
+%% Use On-Off bit loading
+lastBin = 460;
+
+% Convert BMP image to bitstream
+[bitStream, imageData, colorMap, imageSize, bitsPerPixel] = imagetobitstream('image.bmp');
+
+% QAM modulation
+qam_order = 256;
+qamStream = qam_mod(bitStream, qam_order);
+
+% OFDM modulation
+N= 1024;
+L = 120; %length impulse response;
+ofdmStream = ofdm_mod(qamStream, N, L, lastBin);
+
+% Channel
+SNR = 35;
+rxOfdmStream = fftfilt(h,ofdmStream);
+rxOfdmStreamWithNoise = awgn(rxOfdmStream, SNR, 'measured');
+
+% OFDM demodulation
+rxQamStream = ofdm_demod(rxOfdmStreamWithNoise, N, L, h, lastBin);
+
+% QAM demodulation
+rxBitStream = qam_demod(rxQamStream, qam_order);
+
+% Compute BER
+[~,berTransmission] = ber(bitStream,rxBitStream);
+
+% Construct image from bitstream
+imageRx = bitstreamtoimage(rxBitStream, imageSize, bitsPerPixel);
+
+% Plot images
+subplot(2,1,1); colormap(colorMap); image(imageData); axis image; title('Original image'); drawnow;
+subplot(2,1,2); colormap(colorMap); image(imageRx); axis image; title(['Received image']); drawnow;
+
+%% Use adaptive bit loading
 noiseInTime = rxOfdmStreamWithNoise - rxOfdmStream;
 noiseQam = ofdm_demod(noiseInTime, N, L);
 noiseQam = reshape(noiseQam, [N/2-1 length(noiseQam)/(N/2-1)]);
