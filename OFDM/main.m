00632620f7ed66c179f9046aad7505494e174dd3
Jochem Hoes <r0666420@kuleuven.be>
2019-11-04 20:32:21 +0100

    adaptive bit loading

--- OFDM/main.m
+++ OFDM/main.m
@@ -18,10 +18,10 @@ SNR = 20; %addes noise snr
 % h = fir1(100, 0.3, 'low');
 % h = rand(L,1);
 rxOfdmStream = fftfilt(h,ofdmStream);
-rxOfdmStream = awgn(rxOfdmStream, SNR, 'measured');
+rxOfdmStreamWithNoise = awgn(rxOfdmStream, SNR, 'measured');
 
 % OFDM demodulation
-rxQamStream = ofdm_demod(rxOfdmStream, N, L, h);
+rxQamStream = ofdm_demod(rxOfdmStreamWithNoise, N, L, h);
 
 % QAM demodulation
 rxBitStream = qam_demod(rxQamStream, qam_order);
@@ -38,19 +38,57 @@ subplot(2,1,2); colormap(colorMap); image(imageRx); axis image; title(['Received
 
 %% BER per bin
 res = zeros(N,1);
-ratio = zeros(N,1);
+M = log2(qam_order);
 for k = 1:length(qamStream)/N
     for i = 1:N
-       rx = rxBitStream((i-1)*8+1+(k-1)*N*8:i*8+(k-1)*N*8);
-       tx = bitStream((i-1)*8+1+(k-1)*N*8:i*8+(k-1)*N*8);
-       [t,p] = ber(rx,tx);
+       rx = rxBitStream((i-1)*M+1+(k-1)*N*M:i*M+(k-1)*N*M);
+       tx = bitStream((i-1)*M+1+(k-1)*N*M:i*M+(k-1)*N*M);
+       [t,~] = ber(rx,tx);
        res(i) = res(i) + t;
-       ratio(i) = ratio(i) + p;
     end
 end
-%%
 figure
-subplot(2,1,1)
-plot(abs(fft(h)));
-subplot(2,1,2)
-plot(ratio);
+plot(res);
+%% Optimal constellations
+noiseInTime = rxOfdmStreamWithNoise - rxOfdmStream;
+noiseQamStream = ofdm_demod(noiseInTime, N, L, h);
+noiseQamStream = reshape(noiseQamStream, [(N/2-1) length(noiseQamStream)/(N/2-1)]);
+Pn = mean(abs(noiseQamStream).^2,2);
+% semilogy(Pn);
+
+% QAM modulation
+H = fft(h,N-3);
+H = H(1:N/2-1);
+qam_order = floor(log2(1+abs(H).^2 ./ (10*Pn)));
+qamStream = qam_mod(bitStream, qam_order);
+
+% OFDM modulation
+N= 512;
+L = 120; %length impulse response;
+ofdmStream = ofdm_mod(qamStream, N, L);
+
+% Channel
+SNR = 20; %addes noise snr
+% rxOfdmStream = awgn(ofdmStream, SNR);
+% h = fir1(100, 0.3, 'low');
+% h = rand(L,1);
+rxOfdmStream = fftfilt(h,ofdmStream);
+rxOfdmStreamWithNoise = awgn(rxOfdmStream, SNR, 'measured');
+
+% OFDM demodulation
+rxQamStream = ofdm_demod(rxOfdmStreamWithNoise, N, L, h);
+
+% QAM demodulation
+rxBitStream = qam_demod(rxQamStream, qam_order);
+
+% Compute BER
+[~,berTransmission] = ber(bitStream,rxBitStream);
+
+% Construct image from bitstream
+imageRx = bitstreamtoimage(rxBitStream, imageSize, bitsPerPixel);
+
+% Plot images
+subplot(2,1,1); colormap(colorMap); image(imageData); axis image; title('Original image'); drawnow;
+subplot(2,1,2); colormap(colorMap); image(imageRx); axis image; title(['Received image']); drawnow;
+
+
--- OFDM/qam_mod.m
+++ OFDM/qam_mod.m
@@ -1,5 +1,23 @@
 function [qam] = qam_mod(inputSig,order)
-
-%  inputSig = round(rescale(inputSig, 0, order-1));
- qam = qammod(inputSig,order,'InputType','bit','UnitAveragePower',true);
+    if (length(order) == 1)
+        qam = qammod(inputSig,order,'InputType','bit','UnitAveragePower',true);
+    else
+        % Group bits per bin
+        nbCols = sum(order);
+        nbRows = ceil(length(inputSig) / nbCols);
+        % Pad inputSig
+        inputSig = [inputSig;zeros(nbRows * nbCols - length(inputSig),1)];
+        inputSig = transpose(reshape(inputSig, [nbCols nbRows]));
+        qam = zeros(nbRows, length(order));
+        for i = 1:length(order)
+            start = sum(order(1:i)) - order(i)+1;
+            stop = start + order(i) -1;
+            fBin = inputSig(:,start:stop)';
+            fBin = fBin(:);
+            qamForBin = qammod(fBin,2^order(i),'InputType','bit','UnitAveragePower',true);
+            qam(:,i) = qamForBin;
+        end
+        qam = transpose(qam);
+        qam = qam(:);
+    end
 end
