71c97673744b7031ef822783a3043752d0e2424e
HoesJ <33027479+HoesJ@users.noreply.github.com>
2019-11-05 10:22:18 +0100

    minor changes

--- Milestones/ofdm_adapt_bitloading.m
+++ Milestones/ofdm_adapt_bitloading.m
@@ -2,6 +2,7 @@ SNR = 25;
 qam_order_default = 64;
 N_default = 1024;
 L_default = 120;
+gamma = 10;
 load('h_channel.mat')
 %% Simple transmssion
 
@@ -37,44 +38,38 @@ imageRx = bitstreamtoimage(rxBitStream, imageSize, bitsPerPixel);
 pics = figure;
 subplot(2,2,1); colormap(colorMap); image(imageData); axis image; title('Original image'); drawnow;
 subplot(2,2,2); colormap(colorMap); image(imageRx); axis image; title(strcat('Simple transmission -- ',num2str(berTransmission))); drawnow;
+%% Use adaptive bit loading
+noiseInTime = rxOfdmStreamWithNoise - rxOfdmStream;
+noiseQam = ofdm_demod(noiseInTime, N, L);
+noiseQam = reshape(noiseQam, [N/2-1 length(noiseQam)/(N/2-1)]);
+Pn = mean(abs(noiseQam).^2,2);
 
-%% BER per bin
-res = zeros(N,1);
-M = log2(qam_order);
-for k = 1:length(qamStream)/N
-    for i = 1:N
-       rx = rxBitStream((i-1)*M+1+(k-1)*N*M:i*M+(k-1)*N*M);
-       tx = bitStream((i-1)*M+1+(k-1)*N*M:i*M+(k-1)*N*M);
-       [t,~] = ber(rx,tx);
-       res(i) = res(i) + t;
-    end
-end
-aid = figure;
-subplot(2,1,1);
-plot(res)
-title('BER per frequency bin');
+H = fft(h,N-3);
+H = H(1:N/2-1);
 
-%% Use On-Off bit loading
-lastBin = 450;
+b = floor(log2(1+(abs(H).^2) ./ (gamma*Pn)));
+figure
+plot(b);
+title('b');
 
 % Convert BMP image to bitstream
 [bitStream, imageData, colorMap, imageSize, bitsPerPixel] = imagetobitstream('image.bmp');
 
 % QAM modulation
-qam_order = qam_order_default;
+qam_order = b;
 qamStream = qam_mod(bitStream, qam_order);
 
 % OFDM modulation
 N = N_default;
 L = L_default;
-ofdmStream = ofdm_mod(qamStream, N, L, lastBin);
+ofdmStream = ofdm_mod(qamStream, N, L);
 
 % Channel
 rxOfdmStream = fftfilt(h,ofdmStream);
 rxOfdmStreamWithNoise = awgn(rxOfdmStream, SNR, 'measured');
 
 % OFDM demodulation
-rxQamStream = ofdm_demod(rxOfdmStreamWithNoise, N, L, h, lastBin);
+rxQamStream = ofdm_demod(rxOfdmStreamWithNoise, N, L, h);
 
 % QAM demodulation
 rxBitStream = qam_demod(rxQamStream, qam_order);
@@ -87,4 +82,5 @@ imageRx = bitstreamtoimage(rxBitStream, imageSize, bitsPerPixel);
 
 % Plot images
 figure(pics);
-subplot(2,2,3); colormap(colorMap); image(imageRx); axis image; title(strcat('On-Off bit loading -- ',num2str(berTransmission))); drawnow;
+subplot(2,2,3); colormap(colorMap); image(imageRx); axis image; title(strcat('Adaptive bit loading -- ',num2str(berTransmission))); drawnow;
+
