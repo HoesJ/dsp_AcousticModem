30e714d465759c1c4c2ee1521ff31842d9d8c124
Quinten Brosens <r0680377@kuleuven.be>
2019-11-26 10:11:22 +0100

    alternating

--- OFDM alternating training frames/main.m
+++ OFDM alternating training frames/main.m
@@ -1,29 +1,38 @@
-SNR = 25;
-qam_order_default = 64;
+fs =16000;
+qam_order_default = 16;
 N_default = 1024;
-L_default = 120;
-gamma = 10;
-load('h_channel.mat')
-%% Simple transmssion
+L_default = 320;
 
-% Convert BMP image to bitstream
+%% Simple transmssion
+M = qam_order_default;
+N = N_default;
+L = L_default;
+
+Lt = 2;
+Ld = 2;
+
+% Convert; BMP image to bitstream
 [bitStream, imageData, colorMap, imageSize, bitsPerPixel] = imagetobitstream('image.bmp');
+
+%training block generation
+trainblock = randi([0,1],(N/2-1)*log2(M),1);
+qam_trainblock = qam_mod(trainblock,M);
 
 % QAM modulation
 qam_order = qam_order_default;
 qamStream = qam_mod(bitStream, qam_order);
 
 % OFDM modulation
-N = N_default;
-L = L_default;
-ofdmStream = ofdm_mod(qamStream, N, L);
+ofdmStream = ofdm_mod(qamStream, N, L, qam_trainblock, Lt, Ld);
 
 % Channel
-rxOfdmStream = fftfilt(h,ofdmStream);
-rxOfdmStreamWithNoise = awgn(rxOfdmStream, SNR, 'measured');
+[simin,nbsecs,fs,pulse]=initparams(ofdmstream,fs, L);
+sim('recplay');
+out = simout.signals.values;
+[rxOfdmStream,~] = alignIO(out,pulse);
 
 % OFDM demodulation
-rxQamStream = ofdm_demod_legacy(rxOfdmStreamWithNoise, N, L, h);
+rxQamStream = ofdm_demod(rxOfdmStream, N, L, h);
 
 % QAM demodulation
 rxBitStream = qam_demod(rxQamStream, qam_order);
@@ -39,101 +48,4 @@ pics = figure;
 subplot(2,2,1); colormap(colorMap); image(imageData); axis image; title('Original image'); drawnow;
 subplot(2,2,2); colormap(colorMap); image(imageRx); axis image; title(strcat('Simple transmission -- ',num2str(berTransmission))); drawnow;
 
-%% BER per bin
-res = zeros(N,1);
-M = log2(qam_order);
-for k = 1:length(qamStream)/N
-    for i = 1:N
-       rx = rxBitStream((i-1)*M+1+(k-1)*N*M:i*M+(k-1)*N*M);
-       tx = bitStream((i-1)*M+1+(k-1)*N*M:i*M+(k-1)*N*M);
-       [t,~] = ber(rx,tx);
-       res(i) = res(i) + t;
-    end
-end
-aid = figure;
-subplot(2,1,1);
-plot(res)
-title('BER per frequency bin');
-
-%% Use On-Off bit loading
-lastBin = 450;
-
-% Convert BMP image to bitstream
-[bitStream, imageData, colorMap, imageSize, bitsPerPixel] = imagetobitstream('image.bmp');
-
-% QAM modulation
-qam_order = qam_order_default;
-qamStream = qam_mod(bitStream, qam_order);
-
-% OFDM modulation
-N = N_default;
-L = L_default;
-ofdmStream = ofdm_mod(qamStream, N, L, lastBin);
-
-% Channel
-rxOfdmStream = fftfilt(h,ofdmStream);
-rxOfdmStreamWithNoise = awgn(rxOfdmStream, SNR, 'measured');
-
-% OFDM demodulation
-rxQamStream = ofdm_demod_legacy(rxOfdmStreamWithNoise, N, L, h, lastBin);
-
-% QAM demodulation
-rxBitStream = qam_demod(rxQamStream, qam_order);
-
-% Compute BER
-[~,berTransmission] = ber(bitStream,rxBitStream);
-
-% Construct image from bitstream
-imageRx = bitstreamtoimage(rxBitStream, imageSize, bitsPerPixel);
-
-% Plot images
-figure(pics);
-subplot(2,2,3); colormap(colorMap); image(imageRx); axis image; title(strcat('On-Off bit loading -- ',num2str(berTransmission))); drawnow;
-
-%% Use adaptive bit loading
-noiseInTime = rxOfdmStreamWithNoise - rxOfdmStream;
-noiseQam = ofdm_demod_legacy(noiseInTime, N, L);
-noiseQam = reshape(noiseQam, [N/2-1 length(noiseQam)/(N/2-1)]);
-Pn = mean(abs(noiseQam).^2,2);
-
-H = fft(h,N-3);
-H = H(1:N/2-1);
-
-b = floor(log2(1+(abs(H).^2) ./ (gamma*Pn)));
-figure(aid);
-subplot(2,1,2);
-plot(b);
-title('b');
-
-% Convert BMP image to bitstream
-[bitStream, imageData, colorMap, imageSize, bitsPerPixel] = imagetobitstream('image.bmp');
-
-% QAM modulation
-qam_order = b;
-qamStream = qam_mod(bitStream, qam_order);
-
-% OFDM modulation
-N = N_default;
-L = L_default;
-ofdmStream = ofdm_mod(qamStream, N, L);
-
-% Channel
-rxOfdmStream = fftfilt(h,ofdmStream);
-rxOfdmStreamWithNoise = awgn(rxOfdmStream, SNR, 'measured');
-
-% OFDM demodulation
-rxQamStream = ofdm_demod_legacy(rxOfdmStreamWithNoise, N, L, h);
-
-% QAM demodulation
-rxBitStream = qam_demod(rxQamStream, qam_order);
-
-% Compute BER
-[~,berTransmission] = ber(bitStream,rxBitStream);
-
-% Construct image from bitstream
-imageRx = bitstreamtoimage(rxBitStream, imageSize, bitsPerPixel);
-
-% Plot images
-figure(pics);
-subplot(2,2,4); colormap(colorMap); image(imageRx); axis image; title(strcat('Adaptive bit loading -- ',num2str(berTransmission))); drawnow;
 
--- OFDM alternating training frames/ofdm_demod.m
+++ OFDM alternating training frames/ofdm_demod.m
@@ -1,4 +1,4 @@
-function [qamsig,H] = ofdm_demod(ofdm_seq,N,L,trainblock,trainbins)
+function [qamsig,H] = ofdm_demod(ofdm_seq,N,L,trainblock,Lt,Ld)
     % Padd  signal
     ofdm_seq = [ofdm_seq;zeros(ceil(length(ofdm_seq) / (N+L)) * (N+L) - length(ofdm_seq),1)];
 
@@ -15,22 +15,15 @@ function [qamsig,H] = ofdm_demod(ofdm_seq,N,L,trainblock,trainbins)
     qamsig = qamsig(2:N/2,:);
     
     % Channel estimation
-    if nargin < 5
-        trainbins = 1:(N/2-1);
-    end
-    H = zeros(N/2-1,1);
-    count = 1;
-    for i = trainbins
-        A = transpose(qamsig(i,:));
-        b = repmat(trainblock(count), length(A),1);
-        H(i) = b\A;
-        count = count + 1;
-    end
-    if nargin > 4 % perform interpolation        
-        h = ifft([0;H;0;conj(H)]);
-        h(L+1:end) = 0;
-        H = fft(h);
-        H = 2*H(2:512); %% *2 omdat we vermogen hebben weggeknipt en de schaal moet terug gaan
+    num_processing_blocks = size(qamsig,2)/(Lt+Ld);
+    H = zeros(N/2-1, num_processing_blocks);
+    for i = 1:num_processing_blocks
+        for j = 1:length(trainblock)
+            A = transpose(qamsig(i,(i*(Lt+Ld)+1):(i*(Lt+Ld)+1+L) ));
+            b = repmat(trainblock(count), length(A),1);
+            H(i) = b\A;
+            count = count + 1;
+        end
     end
 
     % Channel equalisation
