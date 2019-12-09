b52abc1d2671acd4f360975cf0a310fd6e639520
Quinten Brosens <r0680377@kuleuven.be>
2019-12-09 15:52:23 +0100

    full stereo

--- OFDM - stereo/fixed_transmitter_side_beamformer.m
+++ OFDM - stereo/fixed_transmitter_side_beamformer.m
@@ -1,9 +1,4 @@
-function [a,b, H1and2] = fixed_transmitter_side_beamformer(h1,h2,N)
-
-    H1 = fft(h1,N);
-    H2 = fft(h2,N);
-    H1 = H1(2:512);
-    H2 = H2(2:512);
+function [a,b, H1and2] = fixed_transmitter_side_beamformer(H1,H2)
 
     a = conj(H1)./ sqrt(H1.*conj(H1)+H2.*conj(H2));
     b = conj(H2)./ sqrt(H1.*conj(H1)+H2.*conj(H2));
@@ -14,22 +9,4 @@ function [a,b, H1and2] = fixed_transmitter_side_beamformer(h1,h2,N)
     end
     
     H1and2 = sqrt(H1.*conj(H1)+H2.*conj(H2));
-%     h1and2 = ifft(H1and2);
-%     h1and2 = h1and2(1:round(length(h1and2)/2),1);
-%     H1and2 = 2*fft(h1and2, length(H1and2));
 end
-
-%%code that did not do the job
-
-%     a = zeros(fBins,1);
-%     b = zeros(fBins,1);
-%     for k = 1:fBins
-%         a(k,1) = conj(H1(k,1))/ sqrt(H1(k,1)*conj(H1(k,1)+H2(k,1)*conj(H2(k,1))));
-%         b(k,1) = conj(H2(k,1))/ sqrt(H1(k,1)*conj(H1(k,1)+H2(k,1)*conj(H2(k,1))));
-% 
-% 
-%         sumAB  = sqrt(a(k,1)*conj(a(k,1))+ b(k,1)*conj(b(k,1)));
-%         if(sumAB ~= 1)
-%             error('a and b do not add up to 1');
-%         end
-%     end
--- OFDM - stereo/ofdm_demod_stereo.m
+++ OFDM - stereo/ofdm_demod_stereo.m
@@ -29,16 +29,16 @@ function [qamsig,H] = ofdm_demod(ofdm_seq,N,L,trainblock,Lt,a,b,mu,alpha,qam_ord
 %     H = awgn(H, 10, 'measured');
     
     % Adaptive filtering
-%     [W, filteredOutput] = adaptive_channel_filter(qamsig(:,Lt+1:end),conj(1./H),mu,alpha,qam_order);
-%     H = [H,1./conj(W)];
+    [W, filteredOutput] = adaptive_channel_filter(qamsig(:,Lt+1:end),conj(1./H),mu,alpha,qam_order);
+    H = [H,1./conj(W)];
     % Direct extraction
-    filteredOutput = diag(H) \ qamsig;
+%     filteredOutput = diag(H) \ qamsig;
     
     % remove a and b
     
     
     % data extraction
-    datasig = filteredOutput(usedbins,Lt+1:end);
+    datasig = filteredOutput(usedbins,:s);
     
     % reshape to a line
     qamsig = datasig(:);    
